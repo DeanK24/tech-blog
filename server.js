a1e470b047f4dc18033beffa5615823df74d3615
Dean <deank242@gmail.com>
2022-05-31 20:29:10 -0400

    fixed all errors with handlbars

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
-const path = require('path');
 const express = require('express');
-const exphbs = require('express-handlebars');
 const routes = require('./controllers/');
-const session = require('express-session');
+const sequelize = require('./config/connection');
+const path = require('path');
+
 const helpers = require('./utils/helpers');
+
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({ helpers });
+
+const session = require('express-session');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
@@ -23,14 +27,12 @@ const sess = {
 
 app.use(session(sess));
 
-const hbs = exphbs.create({});
-
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 // turn on routes
 app.use(routes);
